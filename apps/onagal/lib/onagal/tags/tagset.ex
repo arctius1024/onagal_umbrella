767001614af8788b2217f1dc75d7ecf4f0c275b9
Sean Sawyer <ssawyer1024@gmail.com>
2022-09-11 18:55:23 -0400

    Updated tags/tagset to allow put_assoc to overwrite tags/tagset linkings (same as images/tags)

--- apps/onagal/lib/onagal/tags/tagset.ex
+++ apps/onagal/lib/onagal/tags/tagset.ex
@@ -10,15 +10,28 @@ defmodule Onagal.Tags.Tagset do
 
     ## TODO: on_replace may be wrong
     # , on_replace: :delete
-    many_to_many(:tags, Onagal.Tags.Tag, join_through: "tags_tagsets")
+    many_to_many(:tags, Onagal.Tags.Tag, join_through: "tags_tagsets", on_replace: :delete)
   end
 
   @valid_params ~w(name description)a
 
   @doc false
   def changeset(tagset, attrs) do
-    tagset
-    |> cast(attrs, @valid_params)
-    |> validate_required(@valid_params)
+    IO.puts("tagset :changeset")
+    IO.inspect(tagset)
+    IO.inspect(attrs)
+
+    tagset =
+      tagset
+      |> cast(attrs, @valid_params)
+      |> validate_required(@valid_params)
+
+    if Map.get(attrs, :tags),
+      do: tagset |> put_assoc(:tags, attrs.tags, required: false),
+      else: tagset
+
+    if Map.get(attrs, "tags"),
+      do: tagset |> put_assoc(:tags, attrs["tags"], required: false),
+      else: tagset
   end
 end
