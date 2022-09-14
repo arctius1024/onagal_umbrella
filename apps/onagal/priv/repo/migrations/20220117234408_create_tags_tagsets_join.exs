6da570ed83813b8f2c11a707182e2a42f60b37cc
Sean Sawyer <ssawyer1024@gmail.com>
2022-09-13 23:23:00 -0400

    Added on_delete: :delete_all to constraints for join table FKs (retro-fitted existing DB accordingly).

--- apps/onagal/priv/repo/migrations/20220117234408_create_tags_tagsets_join.exs
+++ apps/onagal/priv/repo/migrations/20220117234408_create_tags_tagsets_join.exs
@@ -3,8 +3,8 @@ defmodule Onagal.Persist.Repo.Migrations.CreateTagsTagsetsTable do
 
   def change do
     create table(:tags_tagsets) do
-      add :tag_id, references(:tags)
-      add :tagset_id, references(:tagsets)
+      add :tag_id, references(:tags, on_delete: :delete_all)
+      add :tagset_id, references(:tagsets, on_delete: :delete_all)
     end
 
     create unique_index(:tags_tagsets, [:tag_id, :tagset_id])
--- apps/onagal/priv/repo/migrations/20220727234300_create_filtersets_tags_join.exs
+++ apps/onagal/priv/repo/migrations/20220727234300_create_filtersets_tags_join.exs
@@ -3,8 +3,8 @@ defmodule Onagal.Persist.Repo.Migrations.CreateFiltersetsTags do
 
   def change do
     create table(:filtersets_tags) do
-      add :filterset_id, references(:filtersets)
-      add :tag_id, references(:tags)
+      add :filterset_id, references(:filtersets, on_delete: :delete_all)
+      add :tag_id, references(:tags, on_delete: :delete_all)
     end
 
     create unique_index(:filtersets_tags, [:filterset_id, :tag_id])
