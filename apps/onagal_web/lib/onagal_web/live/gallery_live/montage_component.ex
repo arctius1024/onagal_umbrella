4501f76d1f9cd766ff47b13d5eaaa650d5d69a9a
Sean Sawyer <ssawyer1024@gmail.com>
2022-10-25 16:24:40 -0400

    Implemented some LV 0.18 changes (:if, :for) and refactored montage a bit to clean it up slightly.

--- apps/onagal_web/lib/onagal_web/live/gallery_live/montage_component.ex
+++ apps/onagal_web/lib/onagal_web/live/gallery_live/montage_component.ex
@@ -19,53 +19,58 @@ defmodule OnagalWeb.GalleryLive.MontageComponent do
     ~H"""
     <div>
       <div class="flex flex-row flex-wrap bg-yellow-100">
-        <%= for image <- @images.entries do %>
-          <div class="w-1/6 bg-orange-100 py-1 border-2 border-zinc-400">
-            <.link
-              patch={Routes.gallery_index_path(@socket, :show, image)}
-            >
-              <img id={"image-#{image.id}"}
-                class={image_is_selected(@selected_images, image)}
-                src={thumbnail_for_image(image)}
-                alt={image.original_name}>
-            </.link>
-
-              <button
-                type="button"
-                phx-click="select_image"
-                value={image.id}
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded"
-              >
-                Select
-              </button>
-          </div>
-        <% end %>
+        <div :for={image <- @images.entries} class="w-1/6 bg-orange-100 py-1 border-2 border-zinc-400">
+          <.gallery_image
+            socket={@socket}
+            image={image}
+            selected_images={@selected_images}
+          >
+          </.gallery_image>
+        </div>
       </div>
 
       <div class="flex justify-center">
         <ul class="flex list-style-none">
-          <%= if @images.page_number > 1 do %>
-            <li class="page-item">
-              <.link patch={Routes.gallery_index_path(@socket, :index, page: @images.page_number - 1)}
-                     class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 focus:shadow-none"
-              >
-                &lt;&lt; Prev Page
-              </.link>
-            </li>
-          <% end %>
+          <li :if={@images.page_number > 1} class="page-item">
+            <.link patch={Routes.gallery_index_path(@socket, :index, page: @images.page_number - 1)}
+                    class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 focus:shadow-none"
+            >
+              &lt;&lt; Prev Page
+            </.link>
+          </li>
 
-          <%= if @images.page_number < @images.total_pages do %>
-            <li class="page-item">
-              <.link patch={Routes.gallery_index_path(@socket, :index, page: @images.page_number + 1)}
-                     class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 focus:shadow-none"
-              >
-                Next Page &gt;&gt;
-              </.link>
-            </li>
-          <% end %>
+          <li :if={@images.page_number < @images.total_pages} class="page-item">
+            <.link patch={Routes.gallery_index_path(@socket, :index, page: @images.page_number + 1)}
+                    class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 focus:shadow-none"
+            >
+              Next Page &gt;&gt;
+            </.link>
+          </li>
         </ul>
       </div>
     </div>
+    """
+  end
+
+  def gallery_image(assigns) do
+    ~H"""
+        <.link
+          patch={Routes.gallery_index_path(@socket, :show, @image)}
+        >
+          <img id={"image-#{@image.id}"}
+            class={image_is_selected(@selected_images, @image)}
+            src={thumbnail_for_image(@image)}
+            alt={@image.original_name}>
+        </.link>
+
+          <button
+            type="button"
+            phx-click="select_image"
+            value={@image.id}
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded"
+          >
+            Select
+          </button>
     """
   end
 
